1e2ed48a9aec31d89bd892fcc32d20f7d050e568
martinKim <kcs19542001@hotmail.com>
2023-07-19 22:30:59 +0900

    Feat: 첫 자바 스프링

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,11 @@
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloController {
+
+  @GetMapping("hello")
+  public String hello(Model model) {
+    model.addAttribute("data", "hello!!");
+    return "hello";
+  }
+}
