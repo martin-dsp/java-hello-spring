63c9f9e3fd4517521f503440c9e35f459c7916e2
martinKim <kcs19542001@hotmail.com>
2023-07-29 15:37:59 +0900

    Feat: 자바 코드로 스프링 빈 등록

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,11 +4,14 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 
+//@Service
 public class MemberService {
 
   private final MemberRepository memberRepository;
 
+  @Autowired
   public MemberService(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
